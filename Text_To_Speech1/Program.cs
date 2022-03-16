84b67674efc69d2bcdbaaf1742b50cda170c22d0
soumy <soumy@SAUMYADIP>
2022-03-16 11:14:22 +0530

    Add project files.

---
+++ Text_To_Speech1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Speech.Synthesis;
+
+namespace Text_To_Speech1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SpeechSynthesizer spk = new SpeechSynthesizer();
+            string message = Console.ReadLine();
+            spk.Speak(message);
+            Console.WriteLine("OUTPUT!!!");
+        }
+    }
+}
